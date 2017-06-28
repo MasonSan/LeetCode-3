c513906d9267e05fbb342ff3927d722621298363
Mason Cheng <dada5109@gmail.com>
2017-06-28 09:58:20 +0800

    加入測試專案

---
+++ Longest Substring Without Repeating Characters/Longest Substring Without Repeating Characters Test/TestCase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Longest_Substring_Without_Repeating_Characters_Test
+{
+    [TestClass]
+    public class TestCase
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Longest Substring Without Repeating Characters/Longest Substring Without Repeating Characters/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Longest_Substring_Without_Repeating_Characters
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+
+        }
+    }
+}
